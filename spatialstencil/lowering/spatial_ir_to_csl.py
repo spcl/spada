10d12c75bbbef7a38c4cd13d00687b4210a81881
Tal Ben-Nun <tbennun@gmail.com>
2024-09-23 09:18:21 -0700

    Meeting updates

--- spatialstencil/lowering/spatial_ir_to_csl.py
+++ spatialstencil/lowering/spatial_ir_to_csl.py
@@ -8,34 +8,49 @@ from spatialstencil.syntax.csl import constants as csl
 from spatialstencil.syntax.csl.codefile import CodeFile
 
 
-def lower_spatial_ir_to_csl(kernel: spir.Kernel, rect_size: tuple[int, int] = (64, 64)) -> list[CodeFile]:
+def lower_spatial_ir_to_csl(kernel: spir.Kernel, rect_offset: tuple[int, int] = (0, 0)) -> list[CodeFile]:
     """
     Lowers a routed Spatial IR kernel into Cerebras CSL code.
 
     :param kernel: The Spatial IR kernel to lower.
-    :param rect_size: The size of the output rectangle to use.
+    :param rect_offset: The offset of the output rectangle to use.
     :return: List of code-file objects that can be written to files. See ``write_code_to_files``.
     """
-    # Turn single phase code to be nested in a ``phase``
-    kernel = canonicalization.canonicalize_phases(kernel)
+    # Check if virtual rectangles are equal, consolidate, add phase-end remark at end of computation
+    kernel = canonicalization.inline_phases(kernel)
+    kernel = canonicalization.extend_blocks_to_equivalence_classes(kernel) # Ensure dataflow/compute/place exist
 
     # Prepare outputs
     header = StringIO()
     current_code = StringIO()
     footer = StringIO()
 
-    # TODO: Start with single phase, continue to multi-phase
-    if len(kernel.body) > 1:
-        raise NotImplementedError('Only single-phase kernels are currently supported')
+    # PRECONDITIONS:
+    #     * Rectangles of dataflow/compute/place do not intersect (comes from Spatial IR)
+    #     * For every place block range, the same range should exist for dataflow and compute. (pass)
+    #        * There is no "orphan" block that does not have all matching place/dataflow/compute (pass)
+    #     * There are no phases in the code, there may be local phases for each rectangle (pass)
 
-    # Collect metadata:
+    # Preconditions to verify:
+    #     * All Parameter objects have non-None values
+    #     * All parameters are inlined
+    for param in kernel.parameters:
+        if param.value is None:
+            raise ValueError(f'Undefined parameter value for "{param.name}"')
+
+    # Create mapping between SpIR blocks and PE rectangles
+    #mapping: dict[tuple[int, int], (spir.ComputeBlock | spir.DataflowBlock | spir.PlaceBlock)] = {}
+
+    # For each rectangle, collect metadata:
     #     * Find colors from dataflow blocks
     #     * Collect PE-local arrays from place blocks
     #     * Make (unique) DSDs out of memory accesses in compute blocks
     #     * Generate routing instructions from dataflow blocks
+    #     * Make unique colors out of streams, reduce number of streams
+    #for rect in rectangles:
 
     # Convert compute block subgraphs into tasks:
-    #    * Make dataflow DAG out of computations
+    #    * Make task DAG out of computations
     #    * Any node that has two or more incoming edges (i.e., requires wait) initiates a new task
     #    * Communication inter-task dependency uses ``activate``
     #    * Compute task dependency uses ``unblock``
@@ -44,13 +59,9 @@ def lower_spatial_ir_to_csl(kernel: spir.Kernel, rect_size: tuple[int, int] = (6
     #    * (re)cycle task IDs based on ``csl.{DATA,LOCAL,CONTROL}_TASK_IDS``
 
     # Convert compute blocks' contents:
+    # Preprocessing pass: FMA fusion
     # Communication calls:
     #    * Become async calls
-    # ``phase``:
-    #    * Become a PE-local integer that keeps track of current phase
-    #    * Rebinding tasks (i.e., recycling IDs) between phases becomes switch-case on the variable that maintains
-    #      the current phase
-    #    * Phase end behaves as a dependency on all completions contained in it
     # ``map``:
     #    * becomes DSD operations as much as possible
     #    * @map as a fallback
@@ -58,6 +69,8 @@ def lower_spatial_ir_to_csl(kernel: spir.Kernel, rect_size: tuple[int, int] = (6
     #   * Try to make DSD operations as much as possible
     #   * If index is requested: before unblocking task, set k; inc at end of task
     #   * Wavelet-triggered task as fallback
+    # Rebinding tasks (i.e., recycling IDs) between phases becomes switch-case on the variable that maintains
+    # the current phase
 
     # Generic footer code
     footer.write(f'''
@@ -66,6 +79,9 @@ comptime {{
 }}
 ''')
 
+    rect_size = kernel.get_grid_size()
+
+    scalar_argument_types = []
     # Layout file
     layout_code = f'''
 layout {{
@@ -73,7 +89,7 @@ layout {{
     @set_rectangle{rect_size};
     for (@range(i16, {rect_size[0]})) |pe_x| {{
         for (@range(i16, {rect_size[1]})) |pe_y| {{
-            @set_tile_code(pe_x, pe_y, "code.csl", .{{ .pe_x = pe_x, .pe_y = pe_y }});
+            @set_tile_code(pe_x, pe_y, "code.csl", .{{  }});
         }}
     }}
 
@@ -81,7 +97,7 @@ layout {{
 
     // TODO: Symbol names for arguments and kernel
     // @export_name("", [*]f32, true);
-    @export_name("{kernel.name}", fn()void);
+    @export_name("{kernel.name}", fn({", ".join(scalar_argument_types)})void);
 }}'''
     layout_cf = CodeFile('layout.csl', layout_code)
 
