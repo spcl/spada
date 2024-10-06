5111221beb008a9f0051975e7bc7b64ee51e178c
Tal Ben-Nun <tbennun@gmail.com>
2024-10-06 16:03:13 -0700

    Add constant propagation for Expressions

--- spatialstencil/syntax/spatial_ir/passes.py
+++ spatialstencil/syntax/spatial_ir/passes.py
@@ -1,4 +1,5 @@
 from spatialstencil.syntax.spatial_ir import irnodes as spa
+from spatialstencil.syntax.stencil_ir.type_inference import _result_type_of
 
 
 class Concretizer(spa.NodeTransformer):
@@ -37,3 +38,89 @@ def concretize_parameters(kernel: spa.Kernel, **parameters: int) -> spa.Kernel:
             raise NameError(f'Parameter {param} is not a parameter of kernel {kernel.name}')
 
     return Concretizer(parameters).visit(kernel)
+
+
+class ConstExprPropagation(spa.NodeTransformer):
+    """
+    Propagates constant expressions throughout IR expressions in the code.
+    These include parameters with values and constant literals.
+    """
+
+    def visit_Parameter(self, node: spa.Parameter):
+        if node.value is not None:
+            return spa.ConstantLiteral(node.value, spa.ScalarType.i32)
+        return node
+
+    def visit_UnaryOperator(self, node: spa.UnaryOperator):
+        value: spa.Expression = self.generic_visit(node.value)
+        if isinstance(value.value, spa.ConstantLiteral):
+            restype = _result_type_of(value.value.dtype, optype=node.op)
+            if node.op == '+':
+                cval = +value.value.value
+            elif node.op == '-':
+                cval = -value.value.value
+            else:
+                raise TypeError(f'Unrecognized unary operator "{node.op}"')
+            return spa.ConstantLiteral(cval, restype)
+
+        node.value = value
+        return node
+
+    def visit_BinaryOperator(self, node: spa.BinaryOperator):
+        left: spa.Expression = self.generic_visit(node.left)
+        right: spa.Expression = self.generic_visit(node.right)
+        if isinstance(left.value, spa.ConstantLiteral) and isinstance(right.value, spa.ConstantLiteral):
+            restype = _result_type_of(left.value.dtype, right.value.dtype, optype=node.op)
+            if node.op == '+':
+                result = left.value.value + right.value.value
+            elif node.op == '-':
+                result = left.value.value - right.value.value
+            elif node.op == '*':
+                result = left.value.value * right.value.value
+            elif node.op == '/':
+                result = left.value.value / right.value.value
+            elif node.op == '//':
+                result = left.value.value // right.value.value
+            elif node.op == '%':
+                result = left.value.value % right.value.value
+            elif node.op == '==':
+                result = left.value.value == right.value.value
+            elif node.op == '!=':
+                result = left.value.value == right.value.value
+            elif node.op == '<':
+                result = left.value.value == right.value.value
+            elif node.op == '<=':
+                result = left.value.value == right.value.value
+            elif node.op == '>':
+                result = left.value.value == right.value.value
+            elif node.op == '>=':
+                result = left.value.value == right.value.value
+            else:
+                raise TypeError(f'Unrecognized binary operator "{node.op}"')
+            return spa.ConstantLiteral(result, restype)
+
+        node.left = left
+        node.right = right
+        return node
+
+    def visit_TernaryOperator(self, node: spa.TernaryOperator):
+        cond: spa.Expression = self.generic_visit(node.cond)
+        iftrue: spa.Expression = self.generic_visit(node.if_true)
+        iffalse: spa.Expression = self.generic_visit(node.if_false)
+        if (isinstance(cond.value, spa.ConstantLiteral) and isinstance(iftrue.value, spa.ConstantLiteral) and
+                isinstance(iffalse.value, spa.ConstantLiteral)):
+            restype = _result_type_of(iftrue.value.dtype, iffalse.value.dtype, optype=None)
+            result = iftrue.value.value if cond.value.value else iffalse.value.value
+            return spa.ConstantLiteral(result, restype)
+
+        node.cond = cond
+        node.if_true = iftrue
+        node.if_false = iffalse
+        return node
+
+
+def constexpr_propagation(kernel: spa.Kernel) -> spa.Kernel:
+    """
+    Evaluates constant expressions in a kernel.
+    """
+    return ConstExprPropagation().visit(kernel)
--- tests/test_lowering_spatial_ir_to_csl.py
+++ tests/test_lowering_spatial_ir_to_csl.py
@@ -1,16 +1,26 @@
 import os
 from spatialstencil.lowering.spatial_ir_to_csl import lower_spatial_ir_to_csl
 from spatialstencil.syntax.spatial_ir import parser, passes
+import pytest
+
+
+def test_non_concrete_program():
+    file = os.path.join(os.path.dirname(__file__), '..', 'samples', 'spatial', 'laplacian_routed.sptl')
+    kernel = parser.parse_file(file)
+    with pytest.raises(ValueError, match='parameter value'):
+        lower_spatial_ir_to_csl(kernel)
 
 
 def test_laplacian():
     file = os.path.join(os.path.dirname(__file__), '..', 'samples', 'spatial', 'laplacian_routed.sptl')
     kernel = parser.parse_file(file)
     kernel = passes.concretize_parameters(kernel, I=32, J=32, K=20)
+    kernel = passes.constexpr_propagation(kernel)
     print(kernel.as_ir())
     csl_files = lower_spatial_ir_to_csl(kernel)
     print(csl_files)
 
 
 if __name__ == '__main__':
+    test_non_concrete_program()
     test_laplacian()
