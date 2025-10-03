6851d6d3cebec8a504b230cbae8d7b77190ba807
Tal Ben-Nun <tbennun@gmail.com>
2025-10-03 11:08:43 -0700

    Add laplacian runtime test

---
+++ tests/csl_runtime/test_laplacian.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Compile the spatial stencil program
+sptlc "$SCRIPT_DIR/../../samples/benchmarks/laplacian_4_4_4.sptl" lap_sptl
+
+python <<EOF
+import numpy as np
+a = np.random.rand(6, 6, 4).astype(np.float32)
+np.save('a.npy', a)
+EOF
+
+# Run the compiled program with the Python runtime and the simulator
+timeout -s 9 120 cs_python "$SCRIPT_DIR/../../spatialstencil/runtime/runtime.py" lap_sptl a.npy --benchmark
+
+# Check if the output file matches the expected output
+python $SCRIPT_DIR/laplacian.py a.npy -o expected_out.npy
+python <<EOF
+import numpy as np
+# Load the arrays
+ref = np.load('expected_out.npy')
+output = np.load('OUT__kernel_out_0.npy')
+# Check if the output is correct
+if not np.allclose(output, ref):
+    print("Test failed: Output does not match expected result.")
+    exit(1)
+else:
+    print("Test passed: Output matches expected result.")
+EOF
+
+# Clean up generated files
+rm -rf lap_sptl
+rm -f a.npy OUT__kernel_out_0.npy expected_out.npy
