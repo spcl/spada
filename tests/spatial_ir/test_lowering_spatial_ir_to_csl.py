07b257bcbecb0bf7eef3e1d7dd18b4885889c697
glukas <orionweissnix@gmail.com>
2026-03-28 20:17:39 +0100

    fix tests

--- tests/spatial_ir/test_lowering_spatial_ir_to_csl.py
+++ tests/spatial_ir/test_lowering_spatial_ir_to_csl.py
@@ -5,14 +5,14 @@ import pytest
 
 
 def test_non_concrete_program():
-    file = os.path.join(os.path.dirname(__file__), '..', '..', 'samples', 'spatial', 'add.sptl')
+    file = os.path.join(os.path.dirname(__file__), '..', '..', 'samples', 'spatial', 'blas', 'add.sptl')
     kernel = parser.parse_file(file)
     with pytest.raises(ValueError, match='parameter value'):
         lower_spatial_ir_to_csl(kernel)
 
 
 def test_add():
-    file = os.path.join(os.path.dirname(__file__), '..', '..', 'samples', 'spatial', 'add.sptl')
+    file = os.path.join(os.path.dirname(__file__), '..', '..', 'samples', 'spatial', 'blas', 'add.sptl')
     kernel = parser.parse_file(file)
     kernel = passes.concretize_parameters(kernel, N=32)
     kernel = passes.constexpr_propagation(kernel)
@@ -89,7 +89,7 @@ def test_two_phase_split():
 
 
 def test_laplacian():
-    file = os.path.join(os.path.dirname(__file__), '..', '..', 'samples', 'spatial', 'laplacian_routed.sptl')
+    file = os.path.join(os.path.dirname(__file__), '..', '..', 'samples', 'spatial', 'stencils', 'laplacian_routed.sptl')
     kernel = parser.parse_file(file)
     kernel = passes.constexpr_propagation(kernel)
     print(kernel.as_ir())
@@ -102,7 +102,7 @@ def test_laplacian():
 
 
 def test_forward_sum():
-    file = os.path.join(os.path.dirname(__file__), '..', '..', 'samples', 'spatial', 'forward_sum.sptl')
+    file = os.path.join(os.path.dirname(__file__), '..', '..', 'samples', 'spatial', 'simple', 'forward_sum.sptl')
     kernel = parser.parse_file(file)
     kernel = passes.concretize_parameters(kernel, N=31, K=30)
     kernel = passes.constexpr_propagation(kernel)
